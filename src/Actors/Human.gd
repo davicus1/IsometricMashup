14477063e4204e567f971928101b9b46ca3e26b1
unknown <nm_lamb@hotmail.com>
2021-06-07 11:30:38 -0700

    Added Run BlendSpace Added code to Human gd basic state machine for Idle movement and Pickup

--- src/Actors/Human.gd
+++ src/Actors/Human.gd
@@ -6,6 +6,19 @@ const MOTION_SPEED = 160 * 60 # Pixels/second.
 #puppet var puppet_pos = Vector2()
 puppet var puppet_motion = Vector2()
 
+enum PlayerState{
+	MOVE,
+	PICKUP
+}
+
+var state = PlayerState.MOVE
+var motion = Vector2.ZERO
+var actionDirection = Vector2.DOWN #intention is this is southeast
+
+onready var animationPlayer = $AnimationPlayer
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
+
 #A prototype HU-MAAN. 
 func _init():
 	health_max = 10
@@ -17,20 +30,36 @@ func _init():
 func _ready():
 	if gamestate.is_single_player || is_network_master():
 		$Camera2D.make_current()
-	
+	animationTree.set("parameters/Idle/blend_position", actionDirection)
+	animationTree.set("parameters/Run/blend_position", actionDirection)
+	animationTree.set("parameters/Pickup/blend_position", actionDirection)
 	
 func _physics_process(delta):
-	var motion = Vector2()
+	match state:
+		PlayerState.MOVE:
+			moveState(delta)
+		PlayerState.PICKUP:
+			pickupState(delta)
+
+func moveState(delta):
 	if gamestate.is_single_player || is_network_master():
 		motion.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 		motion.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 		motion.y *= 0.5
-		var speed = MOTION_SPEED
-		if Input.is_action_pressed("Run"):
-			speed *= 3
-		motion = motion.normalized() * speed * delta
-		
+		if motion != Vector2.ZERO:
+			var speed = MOTION_SPEED
+			if Input.is_action_pressed("Run"):
+				speed *= 3
+			actionDirection = motion.normalized()
+			motion = actionDirection * speed * delta
+			animationTree.set("parameters/Idle/blend_position", actionDirection)
+			animationTree.set("parameters/Run/blend_position", actionDirection)
+			animationTree.set("parameters/Pickup/blend_position", actionDirection)
+			animationState.travel("Run")
+		else:
+			animationState.travel("Idle")
 		#Send Network position
+		#TODO check actionDirection animations for multiplayer
 		if not gamestate.is_single_player:
 			rset("puppet_motion", motion)
 		#rset("puppet_pos", position)
@@ -40,9 +69,18 @@ func _physics_process(delta):
 		
 	#warning-ignore:return_value_discarded
 	move_and_slide(motion)
+	if Input.is_action_just_pressed("Pickup"):
+		state = PlayerState.PICKUP
 	
 	#if not is_network_master():
 	#	puppet_pos = position # To avoid jitter (TODO see this in action, copied from bomber)
+
+func pickupState(_delta):
+	motion = Vector2.ZERO
+	animationState.travel("Pickup")
+
+func pickupAnimationFinished():
+	state = PlayerState.MOVE
 
 func set_player_name(new_name):
 	character_name = new_name
