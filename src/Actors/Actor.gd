93781b2d02da46639a570eb0ab351083e28cb1f6
unknown <nm_lamb@hotmail.com>
2021-06-16 10:41:14 -0700

    Moved item interaction code to Actor and created a pass thru / adapter pattern to troll and human.

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -13,3 +13,26 @@ var collectable_items_in_reach:Array = []
 
 func _ready():
 	pass
+
+func on_InteractionArea_area_shape_entered(area_id, area:Area2D, area_shape, self_shape):
+	#TODO Why does this not work when the docs say this is how you find it!????
+	#var obj2 = shape_owner_get_owner(shape_find_owner(area_id))
+	
+	#TODO this seems brittle
+	var owner = area.get_parent()
+	if owner is Item:
+		if owner.collectable:
+			_collectable_item_in_reach(owner)
+
+func _collectable_item_in_reach(item:Item):
+	collectable_items_in_reach.append(item)
+
+func _collectable_item_not_in_reach(item:Item):
+	collectable_items_in_reach.erase(item)
+
+func on_InteractionArea_area_shape_exited(area_id, area, area_shape, self_shape):
+	#TODO this seems brittle
+	var owner = area.get_parent()
+	if owner is Item:
+		if owner.collectable:
+			_collectable_item_not_in_reach(owner)
