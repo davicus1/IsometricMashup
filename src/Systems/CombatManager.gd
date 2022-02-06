9d62d27fdf554e335827fc4442e30011e10391e3
Nicole <nm_lamb@hotmail.com>
2022-02-06 15:18:34 -0800

    Added heal button.

--- src/Systems/CombatManager.gd
+++ src/Systems/CombatManager.gd
@@ -4,7 +4,8 @@ signal Combat_Happened
 enum COMBAT_ACTION{
 	Mele,
 	Ranged,
-	Block
+	Block,
+	Heal
 	}
 	
 func combat_action(action):
@@ -12,6 +13,7 @@ func combat_action(action):
 	var chanceToHit = 10 + difficulty # on a d20
 	var weapon_damage = 0
 	var defence_bonus = 0
+	var healing_points = 0
 	match action:
 		"Mele":
 			weapon_damage = 8
@@ -21,11 +23,15 @@ func combat_action(action):
 			chanceToHit += 2
 		"Block":
 			defence_bonus = 4
+		"Heal":
+			healing_points = 2
 	
 	randomize()
 	
 	if action == "Block":
 		print("Your Action: %s" % action)
+	elif action == "Heal":
+		print("Your Action: %s Healing Points: %s" % [action,healing_points])
 	else:
 		var toHitRoll = (randi() % 20) + 1
 		var damage = 0
